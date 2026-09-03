eb4ef058d4963cc2b3de924ee17a5d1e6e4d68a3
ttyseto-1027 <tty.seto1027@gmail.com>
2026-09-04 04:10:32 +0900

    Bump Develop build for cache update restore

--- app-meta.js
+++ app-meta.js
@@ -3,9 +3,9 @@
   const environment = path.includes('taxi-pay-pwa-develop') ? 'DEVELOP' : 'PRODUCTION';
   window.TAXI_PAY_APP_META = Object.freeze({
     version: '1.4β',
-    build: '20260904-04',
+    build: '20260904-05',
     environment,
-    cacheVersion: 'develop-no-sw-20260904-phase10-04',
-    releasedAtJst: '2026/09/04 03:20:00 JST'
+    cacheVersion: 'taxi-pay-v1.4-beta-20260904-phase10-cache-update-05',
+    releasedAtJst: '2026/09/04 04:12:00 JST'
   });
 })();
