91bef6368ae628ec30c47621dcfcc376a4d78bf1
ttyseto-1027 <tty.seto1027@gmail.com>
2026-09-05 00:45:42 +0900

    Phase 11実装でBuildを20260905-01へ更新

--- app-meta.js
+++ app-meta.js
@@ -3,9 +3,9 @@
   const environment = path.includes('taxi-pay-pwa-develop') ? 'DEVELOP' : 'PRODUCTION';
   window.TAXI_PAY_APP_META = Object.freeze({
     version: '1.4β',
-    build: '20260904-08',
+    build: '20260905-01',
     environment,
-    cacheVersion: 'taxi-pay-v1.4-beta-20260904-phase10-cache-update-08',
-    releasedAtJst: '2026/09/04 05:50:00 JST'
+    cacheVersion: 'taxi-pay-v1.4-beta-20260905-phase11-01',
+    releasedAtJst: '2026/09/05 00:36:00 JST'
   });
 })();
